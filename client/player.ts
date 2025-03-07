dfd8bbf8b075c804b3a8b01af9bdab89db72a4ad
Bryan Chikwendu <26007246+bryanChikwendu@users.noreply.github.com>
2025-03-07 17:08:11 +0000

    Tank Model

--- client/player.ts
+++ client/player.ts
@@ -3,15 +3,20 @@ import * as THREE from 'three';
 export class Player {
     focused;
     speed = 0.05;
-    input = {f: false, b: false};
+    input = {f: false, b: false, l: false, r: false};
     canvas: HTMLCanvasElement;
     sens: number;
     camera: THREE.Camera;
+    dir: number = 90;
+
+    model : THREE.Mesh
     constructor(canvas, sens, camera){
         this.canvas = canvas;
         this.focused = false;
         this.sens = sens;
         this.camera = camera;
+
+       
     }
 
     handleMouseP(event){
@@ -31,6 +36,10 @@ export class Player {
                     this.input.f = false;
                     this.input.b = true;
                     break;
+                case 'a':
+                    this.input.r = false;
+                    this.input.l = true;
+                    break;
                 default:
                     break;
             }
@@ -44,6 +53,10 @@ export class Player {
                     break;
                 case 's':
                     this.input.b = false;
+                    break;
+                case 'a':
+                    this.input.l = false;
+                    break;
                 default:
                     break;
             }
@@ -73,14 +86,54 @@ export class Player {
 
         document.addEventListener('keydown', e => this.handleKeyP(e));
         document.addEventListener('keyup', e => this.handleKeyR(e));
+
+        const geometry = new THREE.BoxGeometry( 0.5, 0.15, 1.2 );
+        const material = new THREE.MeshPhongMaterial( { color: 0xaa2222 } );
+        this.model = new THREE.Mesh( geometry, material );
+        scene.add(this.model);
+    }
+
+    _moveForward(s: number){
+        console.log("Position Before: " + this.camera.position.x + " "
+            + this.camera.position.y + " "
+            + this.camera.position.z
+        )
+        // Calculate "Forward Vector", add it to position * speed
+        var forward = new THREE.Vector3(Math.cos( THREE.MathUtils.degToRad(this.dir)),0,Math.sin(THREE.MathUtils.degToRad(this.dir)));
+        forward.multiplyScalar(s);
+
+        console.log("Forward: " + forward.x + " "
+                                + forward.y + " "
+                                + forward.z
+        )
+        this.camera.position.add(forward);
     }
 
     update(){
+        
+        
         if(this.input.f){
-            this.camera.position.z -= this.speed;
+            this._moveForward(-0.2);
         } else if (this.input.b){
-            this.camera.position.z += this.speed;
+            this._moveForward(0.2);
         }
+        
+        
+        if (this.input.l){
+            this.dir--;
+            if (this.dir < 0){
+                this.dir = 359;
+            }
+        } else if (this.input.r){
+            this.dir++;
+            if (this.dir >= 360){
+                this.dir = 0;
+            }
+        }
+        
+        this.model.position.copy(this.camera.position);
+        this.model.position.z -= 3.5;
+        this.model.position.y -= 1;
     }
 }
 
